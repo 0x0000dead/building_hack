ef74e3d3c2831fd07e50c300f9402acf47aec3f4
Sergey V <41781097+0x0000dead@users.noreply.github.com>
2023-06-24 17:37:19 +0700

    Add files via upload

---
+++ StartPage.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from utils import *
+st.set_page_config(initial_sidebar_state="collapsed",layout='wide', page_title="Стартовая страница", page_icon="🌍")
+# st.title('Wagon Hackathon.')
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+st.columns(2)
+
+url = '/Ввести_вручную'
+for i in range(0, 15):
+    res = st.columns(1)
+
+col4, col5, col6 = st.columns([1, 2, 1])
+
+col6.markdown(f'''
+<a href="{url}"><button style="background-color:#FF7314; color:White; font-size: 40px; border-radius: 10px; border: 3px solid #FF7314; padding: -15px 1112px; text-align: left; text-decoration: none; display: inline-block; margin: -30px -100px; cursor: pointer;">ПОЛУЧИТЬ ПРОГНОЗ</button></a>
+''',
+              unsafe_allow_html=True)
+# invisible button
