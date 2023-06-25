3dc50595136e7f3ab9a0eae6b63aca394c1eb2b1
sergeyvandanov <vandanov2010@gmail.com>
2023-06-25 12:12:27 +0700

    лл

--- pages/Ввести вручную.py
+++ pages/Ввести вручную.py
@@ -1,17 +1,11 @@
+import datetime
+
 import numpy as np
 import pandas as pd
-import streamlit as st
-from model import Predicter
+
 from utils import *
 
 st.set_page_config(initial_sidebar_state="collapsed", layout='wide')
-# st.markdown("# Ввести данные вручную️")
-col = st.columns(7)
-col = st.columns(7)
-col = st.columns(7)
-col = st.columns(7)
-col = st.columns(7)
-col = st.columns(7)
 add_bg_from_local('/app/building_hack/final/16922.png')
 # set color text to black
 st.markdown(
@@ -26,72 +20,77 @@ p {
 )
 
 # create manual input with layout
-col = st.columns(6)
-col1 = st.columns(6)
-col2 = st.columns(7)
+row = st.columns(7)
+row1 = st.columns(6)
+row2 = st.columns(7)
 
 # You can use a column just like st.sidebar:
 st.markdown("Введите данные:")
 
-pr_bl = col[0].text_input('Программа строительства', value='Образование')
-podpr_bl = col[1].text_input('Подпрограмма', value='Общеобразовательные учреждения')
-uniq_id = col[2].text_input('Уникальный айди', value='022-0527')
-identificator = col[3].text_input('Идентификатор вида', value=1.1)
-name_task = col[4].text_input('Название задачи', value='Предпроектные работы')
-percent_end = col[5].text_input('Процент завершения', value=100.0)
-begin = col1[0].text_input('Дата начала задачи', value="2022-01-14")
-end = col1[1].text_input('Дата окончания задачи', value="2023-03-30")
-begin_bp = col1[2].text_input('Дата начала БП0', value="2021-05-04")
-end_bm = col1[3].text_input('Дата окончания БП0', value="2021-05-04")
-status_exp = col1[4].text_input('Статус по экспертизе', value=1.0) # maybe -1 is none, or empty cell
-form_ksg = col1[5].text_input('Дата формирования КСГ', value="2023.01.17") # "-" - meaning none
-expertize = col2[0].selectbox('Экспертиза', ["1ТС", "2ТС","1Т","1С","2С","2Т","-"], index=2)
-code_ds = col2[1].text_input('Код ДС', value='022-0043')
-status_sq = col2[2].selectbox('состояние площадки', ["Свободна, передана", 
-                                                               "Занята",
-                                                                "Свободна, не передана",
-                                                                "Не передана",
-                                                                "Передана под снос (для объектов сноса)",
-                                                                "Не передана под снос (для объектов сноса)",
-                                                                "-"], index=2)
+#obj_num = row[0].text_input('№ п/п', value='36')
+obj_prg = row[0].text_input('Программа строительства', value='Образование')
+obj_subprg = row[1].text_input('Подпрограмма', value='Дошкольные учреждения')
+obj_key = row[2].text_input('obj_key', value='020-0684')
+obj_shortName = row[3].text_input('obj_shortName', value='ДОУ на 125, ТПУ "Мневники"')
+code_task = row[4].text_input('Код задачи', value="1")
+task_name = row[5].text_input('Название задачи', value='Предпроектные работы')
+percent_end = row[6].text_input('Процент завершения задачи', value=100.0, type=float)
 
-genproectir = int(col2[3].text_input('Генпроектировщик', value=1))
-genpodryad = int(col2[4].text_input('Генподрядчик', value=2))
-name_of_worker = col2[5].text_input('Кол-во рабочих', value='wagondigitalnewyearrailway:)happy')
-date_rep = col2[6].text_input('date_report', value='2023.01.20')
+begin = row1[0].date_input('Дата начала задачи', value=datetime.date(2022, 1, 14))
+#end = row1[1].date_input('Дата окончания задачи', value=datetime.date(2023, 3, 30))
+begin_bp = row1[1].date_input('Дата начала БП0', value=datetime.date(2021, 5, 4))
+end_bp = row1[2].date_input('Дата окончания БП0', value=datetime.date(2021, 5, 4))
+status_exp = row1[3].text_input('Статус по экспертизе', value=1.0)  # maybe -1 is none, or empty cell
+expertize = row1[4].selectbox('Экспертиза', ["1ТС", "2ТС", "1Т", "1С", "2С", "2Т", "-"], index=2)
+
+code_ds = row1[5].text_input('Код ДС', value='022-0043')
+status_sq = row1[6].selectbox('состояние площадки', ["Свободна, передана",
+                                                     "Занята",
+                                                     "Свободна, не передана",
+                                                     "Не передана",
+                                                     "Передана под снос (для объектов сноса)",
+                                                     "Не передана под снос (для объектов сноса)",
+                                                     "-"], index=2)
+square = row2[0].text_input('Площадь', value=0.0, type=float)
+genproectir = row2[1].text_input('Генпроектировщик', value=1, type=int)
+genpodryad = row2[2].text_input('Генподрядчик', value=2, type=int)
+workers_number = row2[3].text_input('Кол-во рабочих', value=271.0, type=int)
+date_rep = row2[4].date_input('date_report', value=datetime.date(2023, 1, 20))
 
 # create dataframe
-data_base = {'pr_bl': [pr_bl],
-        'podpr_bl': [podpr_bl],
+#fact_time = (end.timestamp() - begin.timestamp()) / 60 / 60 / 24
+plan_time = (end_bp.timestamp() - begin_bp.timestamp()) / 60 / 60 / 24
 
-        # TODO continue        
-        }
-data_additional = {'pr_bl': [pr_bl],
-        'podpr_bl': [podpr_bl],
+data_base = {'obj_prg': [obj_prg],
+             'obj_subprg': [obj_subprg],
+             'состояние площадки': [status_sq],
+             'ПроцентЗавершенияЗадачи': [percent_end],
+             'plan_time': [plan_time],
+             'Площадь': [square],
+             'Генпроектировщик': [genproectir],
+             'Генподрядчик': [genpodryad],
+             'Кол-во рабочих': [workers_number],
+             }
 
-        # TODO continue        
-        }
 df_base = pd.DataFrame(data_base)
-df_additional = pd.DataFrame(data_additional)
 # st.table(pd.DataFrame(data))
 
 # predict
 if st.button('Предсказать'):
-    #model = Predicter()
-    #y_pred = model.predict(df_base, df_additional)
+    model = Predicter()
+    y_pred = model.predict(df_base, df_additional)
     # df.insert(0, 'y', y_pred)
-    
+
     # append to dataframe
-    # add y_pred col to the beginning of the dataframe
+    # add y_pred row to the beginning of the dataframe
     y_pred = str(np.random.randint(1, 31)) + "." + str(np.random.randint(1, 12)) + "." + "2023"
     # write text Ожидаемое время
 
     # increase text size
-    st.markdown(f'<p style="font-size: 20px;font-color: red">Ожидаемое время окончания строительства составит <u>{str(y_pred)}</u> ч.</p>', unsafe_allow_html=True)
-    #df.insert(0, 'y', y_pred)
+    st.markdown(
+        f'<p style="font-size: 20px;font-color: red">Ожидаемое время окончания строительства составит '
+        f'<u>{str(y_pred)}</u> ч.</p>',
+        unsafe_allow_html=True)
+    # df.insert(0, 'y', y_pred)
     # st.write(df)
     # st.table(pd.DataFrame(df))
-
-
-
-
